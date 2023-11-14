00d589bfc21fa8e620d70873de239968137f1aa0
halljc76 <jchall076@outlook.com>
2023-11-13 21:06:01 -0500

    Adding this just in case

---
+++ makeAnalyses.R
@@ -0,0 +1,14 @@
+#' Include rmarkdown::render code from lecture slides to iterate through the 
+#' levels of education (supplied below). 
+#' 
+#' This might have to be moved to a different file! :)
+for (educ_level in c("Middle or Less", "Some High", "High School Graduate",
+                     "Some College", "College_Graduate")) {
+  rmarkdown::render("./main.Rmd", output_file = paste0(tolower(gsub(" ", "_",
+                                                                    x = educ_level)
+  ),
+  ".html"
+  ),
+  params = list(Education = educ_level)
+  )
+}
